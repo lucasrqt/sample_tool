e28132de39e02d8163894c1531106997af1925bd
Lucas Roquet <lucasroquet@protonmail.com>
2023-08-11 12:11:26 +0000

    mode_sizes.py with torch profile

--- model_sizes.py
+++ model_sizes.py
@@ -3,24 +3,51 @@
 import configs
 import timm
 import torch
+from torch.profiler import profile
+from torchvision.datasets import ImageNet
+from torch.utils.data import DataLoader
 
 
 def main():
     for model_name in configs.MODELS:
         model = timm.create_model(model_name, pretrained=True)
-        model.to("cuda")
+        model = model.to("cuda")
         model.eval()
         
-        param_size = 0
-        for param in model.parameters():
-            param_size += param.nelement() * param.element_size()
+        cfg = timm.data.resolve_data_config({}, model=model)
+        transforms = timm.data.transforms_factory.create_transform(**cfg)
 
-        buffer_size = 0
-        for buffer in model.buffers():
-            buffer_size += buffer.nelement() * buffer.element_size()
+        # initializing the dataset
+        test_set = ImageNet(
+            root=configs.DATA_PATH,
+            transform=transforms,
+            split="val",
+        )
+        # initializing the dataloader
+        data_loader = DataLoader(test_set, batch_size=5)
+        data_iter = iter(data_loader)
+        inputs, label = next(data_iter)
+        inputs = inputs.to("cuda")
 
-        size_all = (param_size + buffer_size) / 1024**2
-        print('{}: {:.3f}MB'.format(model_name, size_all))
+        with profile(activities=[torch.profiler.ProfilerActivity.CUDA],      
+            profile_memory=True,
+            record_shapes=True,) as prof:
+            model(inputs)
+        print(model_name)
+        print(prof.key_averages().table(row_limit=7))
+
+        del inputs, model
+
+        # param_size = 0
+        # for param in model.parameters():
+        #     param_size += param.nelement() * param.element_size()
+
+        # buffer_size = 0
+        # for buffer in model.buffers():
+        #     buffer_size += buffer.nelement() * buffer.element_size()
+
+        # size_all = (param_size + buffer_size) / 1024**2
+        # print('{}: {:.3f}MB'.format(model_name, size_all))
 
 
 if __name__ == '__main__':
